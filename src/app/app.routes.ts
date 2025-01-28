b91709ba1f9d16b2d5764206823871d88fcde2eb
Jose Andres Retamales P <joseandres.retamales@gmail.com>
2025-01-28 13:26:39 -0300

    enrutamiento menu y refrest de contect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { ContactoComponent } from './contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'inicio', component: HomeComponent },
+    { path: 'contacto', component: ContactoComponent },
+
+    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+
+    // Ruta para páginas no encontradas
+    { path: '**', redirectTo: 'inicio' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule {}
